a864f29f90399bfc3ff8edf97ea904b89b129c98
Jin <jinixx@gmail.com>
2014-04-21 17:53:24 +0800

    Transcript panel width issue fix and toggle btn hide on mcms

--- global_assets/lib/transcript.js
+++ global_assets/lib/transcript.js
@@ -1,81 +1,97 @@
-if($("#transcript").length != 0) {
-	//transcript id found
-	console.log("Transcript found");
-	$("#transcript").hide();
-	transcriptTxt = $("#transcript").html();
+if (typeof UIWebView === "undefined") {
+	if($("#legacyTranscript").length != 0) {
+		//transcript id found
+		console.log("Transcript found");
+		$("#legacyTranscript").hide();
+		transcriptTxt = $("#legacyTranscript").html();
 
-	if (transcriptTxt.trim()) {
-		// is not empty nor whitespace
-		console.log("Transcript not empty");
-		var transcriptEnabled = true;
-		var transcriptShown = false;
+		if (transcriptTxt.trim()) {
+			// is not empty nor whitespace
+			console.log("Transcript not empty");
 
-		$("body").append('<div class="transcriptWrapper"><div class="transcriptInnerWrapper"><div class="transcriptbtntoggle globalpos globalLabel"></div></div><div class="transcriptInnerWrapper2"><div class="outOfBox"></div><div class="transcriptPanelBox"><div class="transcriptContent"><!--content here--></div><div class="closebtn"></div></div></div></div>');
+			if (typeof window.transcriptAdded === "undefined") {
 
-		var h = parseInt($(".transcriptInnerWrapper2").css("height"));
-		$(".transcriptInnerWrapper2").css("height", 0);
+				var transcriptEnabled = true;
+				var transcriptShown = false;
 
-		function enableTranscript (b) {
-			if (b != false) {
-				$(".transcriptbtntoggle").css("visibility", "visible");
-			} else {
-				$(".transcriptbtntoggle").css("visibility", "hidden");
-				showTranscript(b);
-			}
-			transcriptEnabled = b;
-		}
+				$("body").append('<div class="transcriptWrapper"><div class="transcriptInnerWrapper"><div class="transcriptbtntoggle globalpos globalLabel"></div></div><div class="transcriptInnerWrapper2"><div class="outOfBox"></div><div class="transcriptPanelBox"><div class="transcriptContent"><!--content here--></div><div class="closebtn"></div></div></div></div>');
 
-		function showTranscript (b) {
-			if (b != false) {
-				//$(".transcriptInnerWrapper2").show();
-				$(".transcriptInnerWrapper2").css("height", h);
-				//$(".outOfBox").show();
-				$(".transcriptPanelBox").addClass("show");
-			} else {
-				$(".transcriptPanelBox").removeClass("show");
-				//$(".outOfBox").hide();
-				$(".transcriptPanelBox").bind("transitionend webkitTransitionEnd oTransitionEnd MSTransitionEnd", function(){
-					//$(".transcriptInnerWrapper2").hide();
-					$(".transcriptInnerWrapper2").css("height", 0);
-					$(".transcriptPanelBox").unbind("transitionend webkitTransitionEnd oTransitionEnd MSTransitionEnd");
+				var h = parseInt($(".transcriptInnerWrapper2").css("height"));
+				$(".transcriptInnerWrapper2").css("height", 0);
+
+				function enableTranscript (b) {
+					if (b != false) {
+						console.log("transcript panel enabled.")
+						$(".transcriptbtntoggle").css("visibility", "visible");
+					} else {
+						console.log("transcript panel disabled.")
+						$(".transcriptbtntoggle").css("visibility", "hidden");
+						showTranscript(b);
+					}
+					transcriptEnabled = b;
+				}
+
+				function showTranscript (b) {
+					if (b != false) {
+						//$(".transcriptInnerWrapper2").show();
+						$(".transcriptInnerWrapper2").css("height", h);
+						//$(".outOfBox").show();
+						$(".transcriptPanelBox").addClass("show");
+					} else {
+						$(".transcriptPanelBox").removeClass("show");
+						//$(".outOfBox").hide();
+						$(".transcriptPanelBox").bind("transitionend webkitTransitionEnd oTransitionEnd MSTransitionEnd", function(){
+							//$(".transcriptInnerWrapper2").hide();
+							$(".transcriptInnerWrapper2").css("height", 0);
+							$(".transcriptPanelBox").unbind("transitionend webkitTransitionEnd oTransitionEnd MSTransitionEnd");
+						});
+					}
+					transcriptShown = b;
+				}
+
+				function toggleTranscript () {
+					if (transcriptShown) {
+						showTranscript (false);
+					} else {
+						showTranscript (true);
+					}
+				}
+
+				$(".transcriptbtntoggle").click (function () {
+					toggleTranscript();
 				});
+
+				$(".transcriptPanelBox .closebtn").click (function () {
+					showTranscript(false);
+				});
+
+				$(".outOfBox").click (function () {
+					showTranscript(false);
+				});
+
+				$(".transcriptContent").html(transcriptTxt);
+
+				enableTranscript(transcriptEnabled);
+
+				window.transcriptAdded = true;
+
 			}
-			transcriptShown = b;
+
+		} else {
+			//transcript content is empty
+			//var transcriptEnabled = false;
+			//var transcriptShown = false;
+
+			//enableTranscript(transcriptEnabled);
+			$(".transcriptbtntoggle").css("visibility", "hidden");
 		}
-
-		function toggleTranscript () {
-			if (transcriptShown) {
-				showTranscript (false);
-			} else {
-				showTranscript (true);
-			}
-		}
-
-		$(".transcriptbtntoggle").click (function () {
-			toggleTranscript();
-		});
-
-		$(".transcriptPanelBox .closebtn").click (function () {
-			showTranscript(false);
-		});
-
-		$(".outOfBox").click (function () {
-			showTranscript(false);
-		});
-
-		$(".transcriptContent").html(transcriptTxt);
-
-		enableTranscript(transcriptEnabled);
-
 	} else {
-		//transcript content is empty
-		//var transcriptEnabled = false;
-		//var transcriptShown = false;
-
-		//enableTranscript(transcriptEnabled);
+		//no transcript id found
 		$(".transcriptbtntoggle").css("visibility", "hidden");
 	}
+
 } else {
-	//no transcript id found
+
 	$(".transcriptbtntoggle").css("visibility", "hidden");
+	
 }
